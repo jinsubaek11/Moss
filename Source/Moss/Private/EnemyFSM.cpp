75db731f5b39efe35d9a6494025d109d05206652
yeni <121008942+guijulim@users.noreply.github.com>
2023-03-07 15:32:03 +0900

    feat: enemy 추가

---
+++ Source/Moss/Private/EnemyFSM.cpp
@@ -0,0 +1,139 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemyFSM.h"
+#include "MainCharacter.h"
+#include "Enemy.h"
+#include <Kismet/GameplayStatics.h>
+#include "MainCharacter.h"
+
+// Sets default values for this component's properties
+UEnemyFSM::UEnemyFSM()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UEnemyFSM::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// 월드에서 MainCharacter 타킷 찾아오기
+	auto actor = UGameplayStatics::GetActorOfClass(GetWorld(), AMainCharacter::StaticClass());
+	// ATPSPlayer 타입으로 캐스팅
+	target = Cast<AMainCharacter>(actor);
+	// 소유 객체 가져오기
+	me = Cast<AEnemy>(GetOwner());
+}
+
+void UEnemyFSM::IdleState()
+{
+	// 1. 시간이 흘렀으니까
+	currentTime += GetWorld()->DeltaRealTimeSeconds;
+	// 1. 만약 경과 시간이 대기 시간을 초과했다면
+	if (currentTime > idleDelayTime)
+	{
+		//3. 이동 상태로 전환하고 싶다.
+		mState = EEnemyState::Move;
+		// 경과 시간 초기화
+		currentTime = 0;
+	}
+}
+
+void UEnemyFSM::MoveState()
+{
+	if (me == nullptr)
+	{
+		return;
+	}
+
+	UE_LOG(LogTemp, Warning, TEXT("kkkkk"))
+
+		// 1. 타깃 목적지가 필요하다.
+		FVector destination = target->GetActorLocation();
+	// 2. 방향이 필요하다.
+	FVector dir = destination - me->GetActorLocation();
+	// 3. 방향으로 이동하고 싶다.
+	me->AddMovementInput(dir.GetSafeNormal());
+
+	// 타깃과 가까워지면 공격 상태로 전환하고 싶다.
+	// 1. 만약 거리가 공격 범위 안에 들어오면
+	if (dir.Size() < attackdRange)
+	{
+		//2. 공격 상태로 전환하고 싶다.
+		mState = EEnemyState::Attack;
+	}
+}
+
+void UEnemyFSM::AttackState()
+{
+	//목표: 일정시간에 한 번씩 공격하고 싶다.
+	//1.시간이 흘러야한다.
+	currentTime += GetWorld()->DeltaRealTimeSeconds;
+	//2. 공격 시간이 됐으니까
+	if (currentTime > attackDelayTime)
+	{
+		// 3.공격하고 싶다.
+		UE_LOG(LogTemp, Warning, TEXT("Attack!!"));
+		// 경과 시간 초기화
+		currentTime = 0;
+	}
+	// 목표: 타킷이 공격 범위를 벗어나면 상태를 이동으로 전환하고 싶다.
+	//1. 타깃과의 거리가 필요하다.
+	float distance = FVector::Distance(target->GetActorLocation(), me->GetActorLocation());
+	//2. 타깃과의 거리가 공격 범위를 벗어났으니까
+	if (distance > attackdRange)
+	{
+		// 3.상태를 이동으로 전환하고 싶다.
+		mState = EEnemyState::Move;
+	}
+}
+
+void UEnemyFSM::DamageState()
+{
+}
+
+void UEnemyFSM::DieState()
+{
+}
+
+void UEnemyFSM::OnDamageProcess()
+{
+	me->Destroy();
+}
+
+
+// Called every frame
+void UEnemyFSM::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	switch (mState)
+	{
+
+	case EEnemyState::Idle:
+		IdleState();
+		break;
+	case EEnemyState::Move:
+		MoveState();
+		break;
+	case EEnemyState::Attack:
+		AttackState();
+		break;
+	case EEnemyState::Damage:
+		DamageState();
+		break;
+	case EEnemyState::Die:
+		DieState();
+		break;
+	}
+
+	//UE_LOG(LogTemp, Warning, TEXT("%d"), mState)
+
+}
+
