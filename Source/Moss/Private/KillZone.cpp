7b9ff47815eee6b4cb0a1c1eb4a3cc0958e43c6d
LimJjiyoung <22314671@naver.com>
2023-03-14 19:47:03 +0900

    killzone 츙돌입력

---
+++ Source/Moss/Private/KillZone.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "KillZone.h"
+#include <Components/BoxComponent.h>
+
+// Sets default values
+AKillZone::AKillZone()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	killBox = CreateDefaultSubobject<UBoxComponent>(TEXT("killBox"));
+	
+	killBox->SetCollisionProfileName(TEXT("PortalPrest"));
+	SetRootComponent(RootComponent);
+
+}
+
+// Called when the game starts or when spawned
+void AKillZone::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	 killBox->OnComponentBeginOverlap.AddDynamic(this, &AKillZone::InKillZone);
+
+}
+
+// Called every frame
+void AKillZone::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AKillZone::InKillZone(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+
+}
+
+
